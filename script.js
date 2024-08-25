bc470f7dce33f0fd5dcedf92fa5d2934c84e6604
carla <carlafzaraujo@gmail.com>
2024-08-25 01:45:12 -0300

    adding the login page and moie player with rating and comments

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 function playVideo() {
-    var thumbnail = document.querySelector('.video-thumbnail');
     var video = document.querySelector('.video');
+    var thumbnail = document.querySelector('.video-thumbnail');
+    var buttons = document.querySelector('.button-container');
+
+    // Mostrar o vídeo e esconder a capa
     thumbnail.style.display = 'none';
+    buttons.style.display = 'none'; // Esconde os botões
     video.style.display = 'block';
-    video.play();
+    video.play(); // Começa a reproduzir o vídeo
+
+    // Adiciona eventos para mostrar os botões quando o vídeo é pausado ou termina
+    video.addEventListener('pause', function() {
+        buttons.style.display = 'flex'; // Mostra os botões
+    });
+
+    video.addEventListener('ended', function() {
+        buttons.style.display = 'flex'; // Mostra os botões quando o vídeo termina
+    });
+}
+
+function rateMovie() {
+    alert("Avalie o filme clicando no botão de avaliação.");
 }
